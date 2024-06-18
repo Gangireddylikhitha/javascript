93044d688e59414b5792bf6670805f1ed81b6cdb
Gangireddylikhitha <gangireddylikhitha916@gmail.com>
2024-06-18 13:22:33 +0530

    add

--- prblm.js
+++ prblm.js
@@ -1,22 +1,22 @@
-// let arr=[1,2,3,4,5,6];
+let n=[1,2,3,4,5,6];
 
-// var sum=0;
-// for(i=0;i<=arr.length;i++){
-//     if(arr[i]%2==0){
-//     sum=sum+arr[i];
-// }
-// }
-// console.log(sum)
+var add=0;
+for(i=0;i<=n.length;i++){
+    if(n[i]%2==0){
+    add=add+n[i];
+}
+}
+console.log(add);
 // o/p  12
 
 /////////////////////////////////////////////////////////////////////////
 
-// let k=1;
-// do{
-//     if(k!=5)
-//     console.log(k);
-//     k++;
-// }while(k<=10)
+let k=1;
+do{
+    if(k!=5)
+    console.log(k);
+    k++;
+}while(k<=10)
 
 // o/p 1
 //     2
@@ -32,19 +32,19 @@
 ///////////////////////////////////////////////////////////////
 
 
-// let num=10;
-// do{
-//     if(num%2==1){
-//         console.log(num);
+let num=10;
+do{
+    if(num%2==1){
+        console.log(num);
         
-//     }
-//     else{
-//         console.log("skipping even number");
+    }
+    else{
+        console.log("skipping even number");
         
-//     }
-//     num--
+    }
+    num--
     
-// }while(num>=1)
+}while(num>=1)
 
 //     skipping even number
 //     9
@@ -57,29 +57,30 @@
 //     skipping even number
 //     1
 
-//  let arr=[10,20,30,40]
-// let sum=0;
-// for(i=0;i<arr.length;i++){
-//     sum=sum+arr[i]; 
+ let arr=[10,20,30,40]
+let sum=0;
+for(i=0;i<arr.length;i++){
+    sum=sum+arr[i]; 
     
-// }
-// let avg=sum/arr.length;
-// if(avg<50){
-//     console.log(avg)
-// }
-// while(avg>50){
-//     console.log("average is large");
-// }
+}
+let avg=sum/arr.length;
+if(avg<50){
+    console.log(avg)
+}
+while(avg>50){
+    console.log("average is large");
+}
 
 // o/p 25
-// let rows=4;
-// for(i=0;i<rows;i++){
-//     star=" ";
-//     for(j=0;j<=i;j++){
-//         star=star+" "+"*";
-//     }
-//     console.log(star);
-// }
+
+let rows=4;
+for(i=0;i<rows;i++){
+    star=" ";
+    for(j=0;j<=i;j++){
+        star=star+" "+"*";
+    }
+    console.log(star);
+}
 // *
 // * *
 // * * *
@@ -114,3 +115,4 @@
  }
 //  rehtaf
 // it is not a pallindrome
+
