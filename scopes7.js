2048c66065ed8410630ce2abae352b257e6adf17
Gangireddylikhitha <gangireddylikhitha916@gmail.com>
2024-06-19 22:52:56 +0530

    add

---
+++ scopes7.js
@@ -0,0 +1,66 @@
+
+// 1. What is the output of:
+
+// var x = 10;
+// function foo() {
+//   console.log(x);
+// }
+// foo();
+
+// Output: 10
+
+// 1. What is the output of:
+
+// var x = 10;
+// function foo() {
+//   var x = 20;
+//   console.log(x);
+// }
+// foo();
+
+// Output: ___20__
+
+// // 1. What is the output of:
+
+// var x = 10;
+// function foo() {
+//   console.log(x);
+//   var x = 20;
+// }
+// foo();
+
+// Output: undefined_____
+
+// // 1. What is the output of:
+
+// var x = 10;
+// function foo() {
+//   console.log(x);
+// }
+// var x = 20;
+// foo();
+
+// Output: _20____
+
+// // 1. What is the output of:
+
+// function foo() {
+//   console.log(x);
+// }
+// var x = 10;
+// foo();
+
+// Output: _10____
+
+// // 1. What is the output of:
+
+// function foo() {
+//   console.log(x);
+// }
+
+// foo();
+// var x = 10;
+
+
+
+// // Output: undefined_____
